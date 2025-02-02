4b2ec27bacc67387bb136761c5e6458b4625f4ee
VDaNT <148575057+NOcodeVDaNT@users.noreply.github.com>
2025-02-02 13:13:47 +0530

    Update jarvis.py

--- python/jarvis.py
+++ python/jarvis.py
@@ -3,6 +3,32 @@ import speech_recognition as sr  # Voice Recognition
 import webbrowser  # Web browsing
 import datetime  # For date and time
 
+from openai import OpenAI
+
+
+
+def deepseek(query):
+    
+
+  client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="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",
+  )
+
+  completion = client.chat.completions.create(
+    
+    model="openai/gpt-3.5-turbo",
+    messages=[
+      {
+        "role": "user",
+        "content": query
+      }
+    ]
+  )
+  answer=completion.choices[0].message.content
+  speak(answer)
+  print(answer)
+
 
 
 def sptext():
@@ -52,9 +78,9 @@ def speak(text):
 
     
 
-    # Print all available voices
-    for voice in voices:
-        print(f"Voice ID: {voice.id}, Name: {voice.name}")
+    # # Print all available voices
+    # for voice in voices:
+    #     print(f"Voice ID: {voice.id}, Name: {voice.name}")
 
 
 #if __name__ == "__main__":This construct allows you to define code that will only run when the script is executed directly, and not when it is imported as a module.
@@ -64,18 +90,40 @@ if __name__ == "__main__":
         
         speak("hi ghost , what can i do for you?")
         
-        data1=sptext().lower()
-        if "your name" in data1:
-            speak("my name is ghost created by another ghost which is GOD for me , nice to meet you")
-        elif "time"in data1:
-            speak("the current time is " + str(datetime.datetime.now().strftime("%H:%M:%S%p")))
-        elif "exit" or "sleep" in data1:
-            speak("ok ghost , i will sleep now , good bye")
-            
-        
+        while True:
+            data1=sptext().lower()
+            if "your name" in data1:
+                speak("my name is ghost created by another ghost which is GOD for me , nice to meet you")
+            elif "time"in data1:
+                speak("the current time is " + str(datetime.datetime.now().strftime("%H:%M:%S%p")))
+            # elif "exit" or "sleep" in data1:
+            #     speak("ok ghost , i will sleep now , good bye")
+            #     break
+            elif "search" in data1:
+                speak("what do you want me to search for?")
+                query=sptext()
+                deepseek(query)
+                
+            else:
+               print("sorry i will not assist you")   
+             
     else:
         print("sorry i will not assist you")    
+            
+            # elif "open google" in data1:
+            #     speak("opening google")
+            #     webbrowser.open("https://www.google.com")
+            # elif "open youtube" in data1:
+            #     speak("opening youtube")
+            #     webbrowser.open("https://www.youtube.com")  
+            # elif "open facebook" in data1:
+            #     speak("opening facebook")
+            #     webbrowser.open("https://www.facebook.com")
         
+      
+            
+        
+    
         
     
 
